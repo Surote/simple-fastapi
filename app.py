4912195b3ba5e9e5e69ba46defb000b55cdc732c
Surote Wongpaiboon <surote.z9@gmail.com>
2025-05-21 12:52:51 +0700

    feat: add prom metrics

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from datetime import datetime
 import pytz
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+
+# Instrument the app with Prometheus metrics
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 async def read_root():
